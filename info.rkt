1b4e30f359651c87aa0241636605cfe70dd08420
Simon Johnston <simonjo@amazon.com>
2018-06-28 15:19:54 -0700

    having to add transitive dependency on "mcfly"

--- info.rkt
+++ info.rkt
@@ -11,5 +11,5 @@
 (define version "1.0")
 (define pkg-authors '(johnstonskj))
 
-(define deps '("base" "csv-reading"))
+(define deps '("base" "csv-reading" "mcfly")) ;; mcfly required by csv-reading
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
