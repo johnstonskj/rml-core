70df208858e9217dbfbb1225c74602db6f8ac538
Simon Johnston <simonjo@amazon.com>
2018-07-03 14:25:14 -0700

    More result-matrix tests

--- rml/test/results.rkt
+++ rml/test/results.rkt
@@ -6,6 +6,8 @@
 ;;
 
 (require rackunit
+         racket/contract
+         (except-in racket/list partition)
          rml/data
          rml/results)
 
@@ -31,23 +33,30 @@
 (test-case
   "make-result-matrix: success"
   (let ([results (make-result-matrix iris-data-set)])
-    (check-equal? (result-value results "Iris-versicolor" "Iris-versicolor") 0)))
+       (check-equal? (result-value results "Iris-versicolor" "Iris-versicolor") 0)))
 
 (test-case
   "result-value: bad classifiers"
   (let ([results (make-result-matrix iris-data-set)])
-    (check-exn exn:fail:contract?
-      (λ () (result-value results "Iris-versicolor" "Not An Iris")))))
+       (check-exn exn:fail:contract?
+         (λ () (result-value results "Iris-versicolor" "Not An Iris")))))
 
 (test-case
   "record-result: success"
   (let ([results (make-result-matrix iris-data-set)])
-  (check-equal? (result-value results "Iris-versicolor" "Iris-versicolor") 0)
-    (record-result results "Iris-versicolor" "Iris-versicolor")
-    (check-equal? (result-value results "Iris-versicolor" "Iris-versicolor") 1)
-    (record-result results "Iris-versicolor" "Iris-versicolor")
-    (check-equal? (result-value results "Iris-versicolor" "Iris-versicolor") 2)
-    (record-result results "Iris-versicolor" "Iris-versicolor")
-    (check-equal? (result-value results "Iris-versicolor" "Iris-versicolor") 3)))
+       (check-equal? (result-value results "Iris-versicolor" "Iris-versicolor") 0)
+       (record-result results "Iris-versicolor" "Iris-versicolor")
+       (check-equal? (result-value results "Iris-versicolor" "Iris-versicolor") 1)
+       (record-result results "Iris-versicolor" "Iris-versicolor")
+       (check-equal? (result-value results "Iris-versicolor" "Iris-versicolor") 2)
+       (record-result results "Iris-versicolor" "Iris-versicolor")
+       (check-equal? (result-value results "Iris-versicolor" "Iris-versicolor") 3)))
 
-;; TODO: tests for result-matrix-formatted
+(test-case
+  "result-matrix-formatted: success"
+  (let* ([results (make-result-matrix iris-data-set)]
+         [printable (result-matrix-formatted results)]
+         [xy (add1 (length (classifier-product iris-data-set)))])
+        (check-eq? (first (first printable)) "true ω pred")
+        (check-eq? (length printable) xy)
+        (check-eq? (length (first printable)) xy)))
