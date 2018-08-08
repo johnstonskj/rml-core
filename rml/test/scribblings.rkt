218e0b1ef7782e543b5f88745949c8fc2f8f1beb
Simon Johnston <simonjo@amazon.com>
2018-08-08 11:39:08 -0700

    Added documentation coverage test

---
+++ rml/test/scribblings.rkt
@@ -0,0 +1,27 @@
+#lang racket
+;;
+;; Racket Machine Learning - Core.
+;;
+;;
+;; ~ Simon Johnston 2018.
+
+;; ---------- Requirements
+
+(require
+  rackunit
+  rackunit/docs-complete)
+
+;; ---------- Test Cases
+
+(test-case
+ "test for documentation completeness"
+ (for ([module (list 'rml/classify
+                     'rml/data
+                     'rml/individual
+                     'rml/not-implemented
+                     'rml/results)])
+  (let ([s (open-output-string)])
+      (parameterize ([current-error-port s])
+        (check-docs module))
+        (displayln (get-output-string s))
+        (check-eq? (string-length (get-output-string s)) 0))))
